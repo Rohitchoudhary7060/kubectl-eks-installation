44fec4a9f9c2edd28175bbad862666411a3564a9
Rohitchoudhary7060 <rohitchoudhary.goldy@gmail.com>
2026-06-06 18:06:04 +0530

    Add script to create EKS cluster and node group

---
+++ create-eks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source variable.env
+envsubst < cluster.yaml.template > cluster.yaml
+eksctl create cluster -f cluster.yaml
+
+envsubst < nodegroup.yaml.template > nodegroup.yaml
+eksctl create nodegroup -f nodegroup.yaml
+
+aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
